3c91841cccd1cd4986f48791ff6ada534addc7a7
Behram-Khattak <behramkttk9@gmail.com>
2024-07-01 03:02:28 -0700

    Added 2 tables to update.php file to create tables in database

--- db-update/update.php
+++ db-update/update.php
@@ -12,5 +12,38 @@ if ($db->query($sql) === TRUE) {
     echo "Error altering table: " . $db->error;
 }
 
+// llx_categorie_extrafields
+$sql2 = "CREATE TABLE llx_categorie_extrafields (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    rowid INT NOT NULL,
+    fk_category INT NOT NULL
+    fieldname VARCHAR(255) NOT NULL
+    created_at TIMESTAMP NOT NULL
+    updated_at TIMESTAMP NOT NULL
+)";
+
+if ($db->query($sql2) === TRUE) {
+    echo "Table llx_categorie_extrafields created successfully";
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
+}
+
+// llx_product_categories    
+$sql2 = "CREATE TABLE llx_product_categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    rowid INT NOT NULL,
+    fk_product INT NOT NULL
+    fieldname VARCHAR(255) NOT NULL
+    fieldvalue VARCHAR(255) NOT NULL
+    created_at TIMESTAMP NOT NULL
+    updated_at TIMESTAMP NOT NULL
+)";
+
+if ($db->query($sql2) === TRUE) {
+    echo "Table llx_product_categories created successfully";
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
+}
+
 // Close connection
 $db->close();
